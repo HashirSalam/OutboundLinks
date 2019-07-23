79627d736c74ebaefe64627bd7af3aa2d32161b4
HashirSalam <hashersalam@gmail.com>
2019-07-23 11:38:16 +0500

    reserved urls

--- OutboundLinks.py
+++ OutboundLinks.py
@@ -4,13 +4,15 @@ import re
 import pandas as pd
 import ssl
 import requests
+from collections import Counter
+pd.options.mode.chained_assignment = None # Removes warning for copied dataframe
 
 
-# def CountFrequency(my_list): 
-#     l=[]
-#     l=my_list.split(' , ')
-#     wordfreq=[l.count(p) for p in l]
-#     print(dict(zip(l,wordfreq)))
+
+
+
+RESERVED = ["gov","wiki"]
+
 
 def getOutboundLink(url):
     headers = {"user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0"}
@@ -28,14 +30,14 @@ def getOutboundLink(url):
     OutboundLinks = list(OutboundLinks)
     return(OutboundLinks)
 
-def getURLbyPhrases(filname):
+def scrapeURLS(filname):
     #print("Reading file please wait ... ")
 
     data = pd.read_csv(filname)
     df = pd.DataFrame(data)
     allPharases =  set([])
    
-    for index, row in df.iterrows():
+    for index,row in df.iterrows():   #dont remove index
        # print(index,row['Related Terms'])
         allPharases.add(row['Related Terms'])
 
@@ -49,18 +51,15 @@ def getURLbyPhrases(filname):
         URLsForPhrase =df.loc[df['Related Terms'] == phrase, 'URL']
         for i in range((URLsForPhrase.shape[0])-1):
             url = df.loc[df['Related Terms'] == phrase, 'URL'].iloc[i]
-            print("Processing for : " + phrase + url)
-            #print(getOutboundLink(url))
-            link = ' , '.join(getOutboundLink(url))
-            LinkPerURL = LinkPerURL.append({'Related Terms': phrase,'URL':url, 'Outbound Links' : link}, ignore_index=True)
-    
-    
-  
-    #print(LinkPerURL)
-    
-   
-    #result = pd.merge(df, LinkPerURL, on='Related Terms') #adding extra rows 
-    #print(result)
+            #print("Processing for : " + phrase + url)
+            
+            if any(x in url for x in RESERVED):
+              
+                LinkPerURL = LinkPerURL.append({'Related Terms': phrase,'URL':url, 'Outbound Links' : url}, ignore_index=True)
+            else:
+                link = ' , '.join(getOutboundLink(url))
+                LinkPerURL = LinkPerURL.append({'Related Terms': phrase,'URL':url, 'Outbound Links' : link}, ignore_index=True)
+        
     LinkPerURL.to_csv("outbound.csv", encoding='utf-8',index=False)
 
 
@@ -68,31 +67,51 @@ def calculateOutboundLinks():
 
     data = pd.read_csv("outbound.csv")
     df = pd.DataFrame(data)
+    
+    #Get those rows with  keywords
+    reserveWordString = '|'.join(RESERVED)
+    
+    reservedRecords = df[df['URL'].str.contains(reserveWordString)==True]
+    
+    diff = df[~df.apply(tuple,1).isin(reservedRecords.apply(tuple,1))]
    
+
     allPharases =  set([])
-   
-    for index, row in df.iterrows():
+    dfObj = pd.DataFrame(columns = ['Related Terms' , 'Outbound Links'])
+    
+    for index,row in diff.iterrows():
        # print(index,row['Related Terms'])
         allPharases.add(row['Related Terms'])
     allPharases = list(allPharases)
-
+    
     for phrase in allPharases:
         LinksPerTerm  = []
-        OutboundLinksForPhrase =df.loc[df['Related Terms'] == phrase, 'Outbound Links']
+        OutboundLinksForPhrase =diff.loc[diff['Related Terms'] == phrase, 'Outbound Links']
+      
         for i in range((OutboundLinksForPhrase.shape[0])-1):
-            links = df.loc[df['Related Terms'] == phrase, 'Outbound Links'].iloc[i]
-            #print(links)
+            links = diff.loc[diff['Related Terms'] == phrase, 'Outbound Links'].iloc[i]
+           
             LinksPerTerm.append(links)
-            # #print(getOutboundLink(url))
-            # link = ' , '.join(getOutboundLink(url))
-            # LinkPerURL = LinkPerURL.append({'Related Terms': phrase,'URL':url, 'Outbound Links' : link}, ignore_index=True)
-        
-        #print(phrase)
-        #print(*LinksPerTerm, sep = "\n") 
+  
         my_lst_str = ''.join(map(str, LinksPerTerm))
         mylist = my_lst_str.split (",")
-        print(mylist)
+       
+        dictPerPhrase = Counter(mylist) #Counts the occurences of each element
+       
+        for link, freq in dictPerPhrase.items(): 
+            if any(x in link for x in RESERVED): #check for wiki and gov 
+                
+                dfObj = dfObj.append({'Related Terms': phrase, 'Outbound Links': link}, ignore_index=True)
+              
+            elif (freq == 2):
+                dfObj = dfObj.append({'Related Terms': phrase, 'Outbound Links': link}, ignore_index=True)
 
+    #print(dfObj)
+    #dfObj.drop_duplicates(subset ="Related Terms", keep = 'first' , inplace = True) #removing duplicates (Keeping first occurance)  
+    result= dfObj.append(reservedRecords, ignore_index = True, sort=False) 
+    
+    result = result[result['Outbound Links'].str.contains('http')]   
+    result.to_csv("Result.csv",encoding='utf-8-sig', index=False, header=True)
 if __name__ == '__main__':
-    #getURLbyPhrases("output.csv")
+    #scrapeURLS("output.csv")
     calculateOutboundLinks()
